7d253b9485bb88b26ee3c2abb13e437551437511
Felipe Porto <felipeporto423@gmail.com>
2026-01-18 19:51:48 -0300

    Projeto Iniciado, Conexao com Banco de Dados Realizado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DoceriaGestao.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
